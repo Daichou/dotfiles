783f55007577c05031d85cf1c986c50dc67ef304
Daichou <tommy0705c@gmail.com>
2016-12-02 14:50:41 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 set -x PYTHONPATH /usr/lib/python3.5/site-packages
 set -x EDITOR vim
-set -x PATH $PATH /usr/bin /usr/sbin /usr/local/bin /opt/android-sdk/platform-tools /opt/android-sdk/tools ~/.cabal/bin/ ~/.local/bin/
+set -x PATH $PATH /usr/bin /usr/sbin /usr/local/bin ~/.cabal/bin/ ~/.local/bin/
 set fish_function_path $fish_function_path "/usr/lib/python3.5/site-packages/powerline/bindings/fish" 
 if begin test $TERM = xterm; or test $TERM = xterm-256color; or test $TERM = screen-256color; end
 	powerline-daemon -q
