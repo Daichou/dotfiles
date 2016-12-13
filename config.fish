0b8600917ea674f1b3619ced0084c4f7d1e0837a
戴宏周 <tommycc@paga.moe>
2016-12-13 10:30:28 +0800

    add autoload .Xresources for urxvt

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 set -x PYTHONPATH /usr/lib/python3.5/site-packages
 set -x EDITOR vim
-set -x PATH $PATH /usr/bin /usr/sbin /usr/local/bin /opt/android-sdk/platform-tools /opt/android-sdk/tools ~/.cabal/bin/ ~/.local/bin/
+set -x PATH $PATH /usr/bin /usr/sbin /usr/local/bin  ~/.cabal/bin/ ~/.local/bin/ ~/.gem/ruby/2.3.0/bin
 set fish_function_path $fish_function_path "/usr/lib/python3.5/site-packages/powerline/bindings/fish" 
 if begin test $TERM = xterm; or test $TERM = xterm-256color; or test $TERM = screen-256color; end
 	powerline-daemon -q
@@ -10,4 +10,6 @@ else
 	set -x LC_ALL en_US.utf-8
 end
 set fish_greeting ""
-
+alias ls='ls --color=auto'
+alias rm='rm -i'
+xrdb ~/.Xresources
