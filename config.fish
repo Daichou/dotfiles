8cc23f88b282cb71fa748453dfd773f9fb1c5c50
戴宏周 <tommycc@paga.moe>
2017-01-17 15:22:21 +0800

    update powerline python version from 3.5 to 3.6

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
-set -x PYTHONPATH /usr/lib/python3.5/site-packages
+set -x PYTHONPATH /usr/lib/python3.6/site-packages
 set -x EDITOR vim
 set -x PATH $PATH /usr/bin /usr/sbin /usr/local/bin  ~/.cabal/bin/ ~/.local/bin/ ~/.gem/ruby/2.3.0/bin
-set fish_function_path $fish_function_path "/usr/lib/python3.5/site-packages/powerline/bindings/fish" 
+set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish" 
 if begin test $TERM = xterm; or test $TERM = xterm-256color; or test $TERM = screen-256color; end
 	powerline-daemon -q
     powerline-setup
